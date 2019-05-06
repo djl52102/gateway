7102d87b605f3d45212c1a0d2bf841a3b6bfb21a
djl52102 <djl52102@foxmail.com>
2019-05-06 17:32:18 +0800

    cloud gateway网关filter 1.添加路由处理后，filter添加请求头 2.路由处理后，filter重写路径

---
+++ src/main/java/cn/ctyun/gateway_three/GatewayThreeApplication.java
@@ -0,0 +1,13 @@
+package cn.ctyun.gateway_three;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayThreeApplication.class, args);
+	}
+
+}
