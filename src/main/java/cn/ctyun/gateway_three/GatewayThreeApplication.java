5cf54ee0dea41396b908a1c89fe65ef4b163c428
djl52102 <djl52102@foxmail.com>
2019-05-06 18:04:28 +0800

    cloud gateway网关filter 1.添加路由处理后，filter添加请求头 2.路由处理后，filter重写路径 3.自定义拦截器，打印请求耗时

--- src/main/java/cn/ctyun/gateway_three/GatewayThreeApplication.java
+++ src/main/java/cn/ctyun/gateway_three/GatewayThreeApplication.java
@@ -2,12 +2,25 @@ package cn.ctyun.gateway_three;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GatewayThreeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayThreeApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator customerRouteLocator(RouteLocatorBuilder builder){
+		return builder.routes().route(
+				r->r.path("/customer/**")
+				.filters(f->f.filter(new RequestTimeFilter()).addResponseHeader("X-Response-Default-Foo","default-Bar"))
+				.uri("http://httpbin.org:80/get").order(0).id("customer_filter_router"))
+		.build();
+	}
 }
